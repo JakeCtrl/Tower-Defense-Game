07f4b2a0a50ec6cac07b20993e15356d0014dfd3
JakeCtrl <jakeinctrl@gmail.com>
2020-02-21 19:53:50 -0500

    Just making our Main class extend application

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,18 @@
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
     public static void main(String args[]){
-        System.out.println("Yes");
+
+        Application.launch(args);
+
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Tower-Defense-Game");
+
+        // Show the stage
+        primaryStage.show();
     }
 }
